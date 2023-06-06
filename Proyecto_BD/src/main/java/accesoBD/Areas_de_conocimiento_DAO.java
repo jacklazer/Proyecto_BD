99b40c61eec197bd845456e0d9f4f2b964500a2f
Juan CV <jacklazerjscv@gmail.com>
2023-06-06 17:09:26 -0500

    Clases del paquete accesoBD (pero sin implementacion)

---
+++ Proyecto_BD/src/main/java/accesoBD/Areas_de_conocimiento_DAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accesoBD;
+
+/**
+ *
+ * @author juanc
+ */
+public class Areas_de_conocimiento_DAO {
+    
+}
